ae46ac27574baeea4213a09542d7b49c0ebdd72e
Christine Tu <christinetu@umail.ucsb.edu>
2023-05-13 14:40:26 -0700

    updated frontend

---
+++ frontend/src/components/filedrop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Filedrop() {
+    return (
+      <div className="filedrop">
+        <p>
+            placeholder dropzone
+        </p>
+      </div>
+    );
+  }
+  
+  export default Filedrop;
