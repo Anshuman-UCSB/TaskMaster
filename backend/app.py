77929e0dc8046abcdf95bf0096d32c271bb03a44
Anshuman Dash <anshuman@ucsb.edu>
2023-05-13 12:42:37 -0700

    read text from pdfs

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,25 @@
+# write a flask app that has one put endpoint
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET'])
+def get():
+	return "Hello World!"
+
+@app.route('/pdf/post', methods=['POST'])
+def postpdf():
+	data = request.get_json()
+	print(data)
+	return ":)"
+
+@app.route('/text/post', methods=['POST'])
+def posttext():
+	data = request.get_json()
+	print(data)
+	return ":)"
+
+if __name__ == '__main__':
+	app.run(debug=True, host='localhost')
+	
