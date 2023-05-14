69c06ae9a557ac473258f140e6dda246ad17b9f3
Esmé Puzio <esmepuzio@169-231-102-8.wireless.ucsb.edu>
2023-05-13 23:06:35 -0700

    First pass at CSS, pivoting to Tailwind

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -25,36 +25,37 @@ function Form() {
       }
     }
 
-    // let input;
-    // if (type === "file") {
-    //     input = <Filedrop />;
-    // } else {
-    //     input = <Textinput onChange={(v) => setText(v.target.value)}/>;
-    // }
+    let input;
+    if (type === "file") {
+        input = <Filedrop />;
+    } else {
+        input = <Textinput onChange={(v) => setText(v.target.value)}/>;
+    }
 
     return (
       <div className="form">
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-      <Grid item xs={12}>
-        <ToggleButtonGroup
-        color="primary"
-        value={type}
-        exclusive
-        onChange={handleChange}
-        aria-label="Platform"
-        >
-            <ToggleButton value="text">Text</ToggleButton>
-            <ToggleButton value="file">File</ToggleButton>
-        </ToggleButtonGroup>
-        </Grid>
-        <Grid item xs={12}>
-        {(type) === "file" && <Filedrop />}
-        {(type) === "text" && <Textinput onChange={(v) => {console.log("changed"); setText(v.target.value);}}/>}
-        </Grid>
-        <Grid item xs={12}>
-        <Button 
-        variant="contained"
-        onClick={submitInfo}>Submit</Button>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} flexDirection="column">
+        <Grid item s={6}>
+          <div className="toggleButtons">
+            <ToggleButtonGroup
+            color="primary"
+            value={type}
+            exclusive
+            onChange={handleChange}
+            // aria-label="Platform"
+            >
+                <ToggleButton value="text">Text</ToggleButton>
+                <ToggleButton value="file">File</ToggleButton>
+            </ToggleButtonGroup>
+          </div>
+          </Grid>
+          <Grid item xs={6} p={2}>
+          <submitButton 
+            onClick={submitInfo}>Submit</submitButton>
+          </Grid>
+          <Grid item m={12}>
+          {(type) === "file" && <Filedrop/>}
+          {(type) === "text" && <Textinput onChange={(v) => {console.log("changed"); setText(v.target.value);}}/>}
         </Grid>
       </Grid>
       </div>
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Login from './login';
+import logo from './images/SBHacks-Logo.png';
 
 function Header() {
     return (
       <div className="header">
         <h1>
-            Placeholder title lol
+          <div class="wrapper">
+            <header1>
+              <div className="logo-image">
+                <img src={logo}/>
+              </div>
+              Taskey
+            </header1>
+          </div>
         </h1>
         <Login />
       </div>
